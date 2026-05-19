bc121cd5335c4d5e587461e3e0cfbde767690731
Stephane Metairie <stephane.metairiev@gmail.com>
2026-05-19 21:00:15 +0200

    docker compose and start ghost

--- push_docker_compose.tf
+++ push_docker_compose.tf
@@ -11,4 +11,18 @@ resource "null_resource" "launch" {
       private_key = file("${path.module}/secrets/id_rsa")
     }
   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /root",
+      "docker-compose up -d",
+      "docker-compose ps"
+    ]
+    connection {
+      type        = "ssh"
+      host        = vultr_instance.current.main_ip
+      user        = "root"
+      private_key = file("${path.module}/secrets/id_rsa")
+    }
+  }
 }
